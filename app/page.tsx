fa6ce02d63f740d2579ffdac25837f029ae48dfc
EverytimeAomsin <sarin542@hotmail.com>
2025-07-23 21:51:23 +0700

    พื้นหลัง เทา

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-black via-gray-900 to-gray-800 relative">
-    <h1
-  className="mt-[-4rem] sm:mt-0 text-6xl sm:text-8xl md:text-9xl font-extralight text-gray-200 mb-8 tracking-[0.1em]"
-  style={{ fontFamily: "Prompt, sans-serif", fontWeight: 300 }}
->
-  เมพิตรี
-</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[#DEDEDE] relative">
+      <h1
+        className="mt-[-4rem] sm:mt-0 text-6xl sm:text-7xl md:text-8xl font-extralight text-white mb-8 tracking-[0.1em]"
+        style={{ fontFamily: "Prompt, sans-serif", fontWeight: 300 }}
+      >
+        เมพิตรี
+      </h1>
 
       <video
         src="/technology-animations-8772922.mp4"
